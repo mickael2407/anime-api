0622ec731c955447668995bdd70cc71c968885de
Mickael <pezzoni.mickael@gmail.com>
2019-04-09 17:08:59 +0200

    clean

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const apiRoutes = require('./api-routes');
 const PORT = process.env.PORT || 8080;
 const HOST_DB = process.env.DB || 'localhost';
-const apiRoutes = require('./api-routes');
 
+mongoose.connect('mongodb://' + HOST_DB + '/animeapi');
+const db = mongoose.connection;
+db.
 
 /* 
-    use nodemon [file.js] to run api
+*    Use nodemon [file.js] to run api
 */
 express.use(bodyParser.urlencoded({
     extended: true
@@ -18,12 +21,8 @@ express.use(bodyParser.json());
 express.get('/', (req, res) => {
     res.send('Welcome');
 });
-
-mongoose.connect('mongodb://' + HOST_DB + '/animeapi');
-var db = mongoose.connection;
-
 express.use('/api',apiRoutes);
 
 express.listen(PORT, () => {
     console.log("Running RestHub on port " + PORT);
-})
+});
